cf2d0d2e971dd66a0690235a9f5811daabec8348
dlewis7444 <dlewis7444@users.noreply.github.com>
2026-03-09 16:24:27 -0500

    feat: add PassphraseDialog

---
+++ ydoit-phase1/ydoit/src/ydoit/passphrase_dialog.py
@@ -0,0 +1,42 @@
+"""Passphrase prompt dialog."""
+
+from __future__ import annotations
+
+import gi
+
+gi.require_version("Gtk", "4.0")
+gi.require_version("Adw", "1")
+from gi.repository import Adw, Gtk
+
+
+class PassphraseDialog(Adw.AlertDialog):
+    """Modal dialog prompting the user for the GPG passphrase."""
+
+    def __init__(self, error_message: str | None = None) -> None:
+        body = (
+            error_message
+            if error_message
+            else "Enter your GPG passphrase to unlock ydoit."
+        )
+        super().__init__(heading="Enter Passphrase", body=body)
+
+        self._entry = Adw.PasswordEntryRow(title="Passphrase")
+
+        box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+        box.set_margin_top(12)
+        box.set_margin_bottom(12)
+        box.set_margin_start(12)
+        box.set_margin_end(12)
+        box.append(self._entry)
+        self.set_extra_child(box)
+
+        self.add_response("cancel", "Cancel")
+        self.add_response("ok", "Unlock")
+        self.set_response_appearance("ok", Adw.ResponseAppearance.SUGGESTED)
+        self.set_default_response("ok")
+        self.set_close_response("cancel")
+
+    @property
+    def passphrase(self) -> str:
+        """Return the text currently in the passphrase field."""
+        return self._entry.get_text()
